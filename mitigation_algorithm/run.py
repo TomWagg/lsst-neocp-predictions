cd66a4b9e712bc344a5eeeca0b248a86b4a8ad89
Tom Wagg <tomjwagg@gmail.com>
2023-04-03 13:28:56 -0700

    new ecl_lat plot without trails

--- mitigation_algorithm/run.py
+++ mitigation_algorithm/run.py
@@ -6,7 +6,7 @@ run_start = time.time()
 for i in range(365):
     print(f"\nStarting night {i}")
     start = time.time()
-    get_detection_probabilities(night_start=i, obj_type="neo")
+    get_detection_probabilities(night_start=i, obj_type="neo", pool_size=38)
     print(f"Time for this run: {time.time() - start:1.1f}s")
 
 print(f"\n\nOverall, it took {time.time() - run_start:1.1f}s")
