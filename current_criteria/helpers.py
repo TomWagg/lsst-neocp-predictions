d569a2b37f274e878c682391cc011f488157c025
Tom Wagg <tomjwagg@gmail.com>
2023-04-21 11:22:31 -0700

    trim helpers code

--- current_criteria/helpers.py
+++ current_criteria/helpers.py
@@ -1,55 +1,7 @@
-import matplotlib.pyplot as plt
-from matplotlib.patches import Patch
 import numpy as np
 import pandas as pd
 from os import listdir
 from os.path import isfile
-
-plt.rc('font', family='serif')
-plt.rcParams['text.usetex'] = False
-fs = 24
-
-# update various fontsizes to match
-params = {'figure.figsize': (12, 8),
-          'legend.fontsize': fs,
-          'axes.labelsize': fs,
-          'xtick.labelsize': 0.6 * fs,
-          'ytick.labelsize': 0.6 * fs,
-          'axes.linewidth': 1.1,
-          'xtick.major.size': 7,
-          'xtick.minor.size': 4,
-          'ytick.major.size': 7,
-          'ytick.minor.size': 4}
-plt.rcParams.update(params)
-
-
-def plot_neo_scores(neo_scores, density=False, bins=np.linspace(0, 100, 30), night=0):
-    fig, ax = plt.subplots(figsize=(12, 8))
-
-    handles = []
-
-    for score in neo_scores:
-        ax.hist(score["scores"], bins=bins, facecolor=score["colour"][:-1] + [0.2],
-                label=score["label"], lw=3, density=density)
-        ax.hist(score["scores"], bins=bins, color=score["colour"], histtype="step",
-                lw=3, density=density)
-
-        handles.append(Patch(facecolor=score["colour"][:-1] + [0.2],
-                             edgecolor=score["colour"],
-                             label=score["label"], lw=3))
-
-    ax.legend(handles=handles, loc="upper center", ncol=2, fontsize=0.8 * fs)
-
-    ax.set_xlabel("NEO Score")
-
-    if density:
-        ax.set_ylabel("Density")
-    else:
-        ax.set_ylabel("Number of objects")
-
-    ax.set_title(f"Night {night}", fontsize=fs)
-
-    return fig, ax
 
 
 def get_specific_neo_score(path, file_name):
