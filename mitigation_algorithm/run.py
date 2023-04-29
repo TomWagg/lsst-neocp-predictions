f820040fa4cdd10aa99d9b423eb26bc3f600cd19
Tom Wagg <tomjwagg@gmail.com>
2023-04-28 17:40:52 -0700

    update final plots

--- mitigation_algorithm/run.py
+++ mitigation_algorithm/run.py
@@ -6,7 +6,7 @@ run_start = time.time()
 for i in range(365):
     print(f"\nStarting night {i}")
     start = time.time()
-    get_detection_probabilities(night_start=i, obj_type="neo", pool_size=38)
+    get_detection_probabilities(night_start=i, obj_type="mba", pool_size=30)
     print(f"Time for this run: {time.time() - start:1.1f}s")
 
 print(f"\n\nOverall, it took {time.time() - run_start:1.1f}s")
