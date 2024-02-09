9d8366521fa8e43cf195d17737103ade5209fc7d
Tom Wagg <tomjwagg@gmail.com>
2024-02-08 22:44:39 -0800

    ignore files that don't exist

--- pipeline/prep_obs_files.py
+++ pipeline/prep_obs_files.py
@@ -8,14 +8,17 @@ sys.path.append("../src")
 import trackletfilter
 
 def prune_night_file(night, path="/epyc/projects/neocp-predictions/output/synthetic_obs"):
-    df = pd.read_hdf(os.path.join(path, f"night_{night:04d}.h5"), key="df")
-    df = df.sort_values(["ObjID", "FieldMJD_TAI"])
-    df.reset_index(inplace=True, drop=True)
+    file_path = os.path.join(path, f"night_{night:04d}.h5")
+    if os.path.isfile(file_path):
+        df = pd.read_hdf(file_path, key="df")
+        df = df.sort_values(["ObjID", "FieldMJD_TAI"])
+        df.reset_index(inplace=True, drop=True)
     
-    filtered_df = trackletfilter.filter_observations(df, min_obs=3, min_arc=1, max_time=90)
-    filtered_df.to_hdf(os.path.join(path, f"filtered_night_{night:04d}.h5"), key="df")
+        filtered_df = trackletfilter.filter_observations(df, min_obs=3, min_arc=1, max_time=90)
+        filtered_df.to_hdf(os.path.join(path, f"filtered_night_{night:04d}.h5"), key="df")
+    print(night, "done")
 
 if __name__ == "__main__":
-    nights = range(1, 366)
-    with Pool(20) as p:
+    nights = range(366)
+    with Pool(30) as p:
         p.map(prune_night_file, nights)
