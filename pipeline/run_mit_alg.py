06ce6eeb10046a01de7b9c9f00db54a39b143202
Tom Wagg <tomjwagg@gmail.com>
2024-06-18 22:45:06 -0700

    allow for missing files

--- pipeline/run_mit_alg.py
+++ pipeline/run_mit_alg.py
@@ -3,10 +3,10 @@ import time
 
 run_start = time.time()
 
-for i in range(365):
+for i in [0]:#range(365):
     print(f"\nStarting night {i}")
     start = time.time()
-    get_detection_probabilities(night_start=i, obj_type="mba", pool_size=30)
+    get_detection_probabilities(night_start=i, pool_size=30, schedule_type='predicted')
     print(f"Time for this run: {time.time() - start:1.1f}s")
 
 print(f"\n\nOverall, it took {time.time() - run_start:1.1f}s")
